73a6bb7f077aab48ec0af543b9171e793bcb1c84
Christian Schlichtherle <christian@schlichtherle.de>
2015-04-04 14:49:07 +0200

    Reorganized UI unit tests.

--- truelicense-ui/src/test/scala/net/java/truelicense/ui/MessagesTest.scala
+++ truelicense-ui/src/test/scala/net/java/truelicense/ui/MessagesTest.scala
@@ -5,11 +5,11 @@
 package net.java.truelicense.ui
 
 import net.java.truelicense.ui.LicenseWizardMessage._
+import net.java.truelicense.ui.MessagesTestSupport._
 import org.junit.runner.RunWith
 import org.scalatest.WordSpec
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.PropertyChecks._
-import net.java.truelicense.core.MessagesTestSupport._
 
 /**
   * @author Christian Schlichtherle
@@ -20,40 +20,40 @@ class MessagesTest extends WordSpec {
   "Messages" should {
     "be binary serializable" in {
       val table = Table(
-        ("key"),
-        (wizard_title),
-        (welcome_title),
-        (welcome_prompt),
-        (welcome_install),
-        (welcome_display),
-        (welcome_uninstall),
-        (install_title),
-        (install_prompt),
-        (install_select),
-        (install_install),
-        (install_success),
-        (install_failure),
-        (install_fileExtension),
-        (install_fileFilter),
-        (display_title),
-        (display_subject),
-        (display_holder),
-        (display_issuer),
-        (display_issued),
-        (display_notBefore),
-        (display_notAfter),
-        (display_consumer),
-        (display_consumerFormat),
-        (display_info),
-        (display_dateTimeFormat),
-        (display_failure),
-        (uninstall_title),
-        (uninstall_prompt),
-        (uninstall_uninstall),
-        (uninstall_success),
-        (uninstall_failure),
-        (failure_title)
-        )
+        "key",
+        wizard_title,
+        welcome_title,
+        welcome_prompt,
+        welcome_install,
+        welcome_display,
+        welcome_uninstall,
+        install_title,
+        install_prompt,
+        install_select,
+        install_install,
+        install_success,
+        install_failure,
+        install_fileExtension,
+        install_fileFilter,
+        display_title,
+        display_subject,
+        display_holder,
+        display_issuer,
+        display_issued,
+        display_notBefore,
+        display_notAfter,
+        display_consumer,
+        display_consumerFormat,
+        display_info,
+        display_dateTimeFormat,
+        display_failure,
+        uninstall_title,
+        uninstall_prompt,
+        uninstall_uninstall,
+        uninstall_success,
+        uninstall_failure,
+        failure_title
+      )
       forAll (table) { key => testSerialization(key format ()) }
     }
   }
