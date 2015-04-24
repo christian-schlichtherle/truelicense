acadae4d60479c553cebc3785276011acc89dceb
Christian Schlichtherle <christian@schlichtherle.de>
2015-04-24 21:02:03 +0200

    Simplifies Javascript.

--- src/site/resources/js/carousel.js
+++ src/site/resources/js/carousel.js
@@ -1,11 +1,10 @@
-!function ($) {
-    $(function () {
-        $('.carousel').carousel();
-        $('.left.carousel-control').on("click", function () {
-            $('.carousel').carousel('prev');
-        });
-        $('.right.carousel-control').on("click", function () {
-            $('.carousel').carousel('next');
-        })
-    })
-}(window.jQuery);
+/*
+ * Copyright (C) 2005-2015 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+
+$(function () {
+    $('.left.carousel-control').attr('data-slide', 'prev');
+    $('.right.carousel-control').attr('data-slide', 'next');
+    $('.carousel').carousel();
+});
