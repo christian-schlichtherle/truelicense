2acc9a7610cc9b49e7c91025b99120f1bb5efd7c
Christian Schlichtherle <christian@schlichtherle.de>
2015-04-24 20:33:46 +0200

    Adds an experimental page with an accordion.

---
+++ truelicense-maven-archetype/src/site/resources/js/accordion.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2005-2015 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+
+$(function () {
+    $('.accordion-toggle').attr('data-toggle', 'collapse');
+});
