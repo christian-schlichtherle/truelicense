3d66765e7c21523efa8ed15e479ff691f4d82921
Christian Schlichtherle <christian@schlichtherle.de>
2015-05-02 14:32:36 +0200

    Adds missing package-info.java.

---
+++ truelicense-v2-commons/src/main/java/org/truelicense/v2/commons/auth/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2005-2015 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+
+/**
+ * Provides authentication services for use with V1 format license keys.
+ *
+ * @author Christian Schlichtherle
+ */
+package org.truelicense.v2.commons.auth;
