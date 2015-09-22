f511fbb4acd090ee14abd50f73cea6b6a6d6a974
Christian Schlichtherle <christian@schlichtherle.de>
2015-09-22 06:28:15 +0200

    Gets rid of some parenthesis.

--- truelicense-it/src/test/scala/net/java/truelicense/it/core/LicenseKeyLifeCycleTestSuite.scala
+++ truelicense-it/src/test/scala/net/java/truelicense/it/core/LicenseKeyLifeCycleTestSuite.scala
@@ -20,16 +20,16 @@ extends WordSpec { this: TestContext =>
 
   def check(license: License, notBefore: Date = null, notAfter: Date = null) {
     import license._
-    getConsumerAmount should be (1)
-    getConsumerType should be ("User")
-    getExtra should be (null)
+    getConsumerAmount shouldBe 1
+    getConsumerType shouldBe "User"
+    getExtra shouldBe null
     getHolder should not be null
-    getInfo should be (null)
+    getInfo shouldBe null
     getIssued should not be null
     getIssuer should not be null
-    getNotAfter should be (notAfter)
-    getNotBefore should be (notBefore)
-    getSubject should be (managementContext.subject)
+    getNotAfter shouldBe notAfter
+    getNotBefore shouldBe notBefore
+    getSubject shouldBe managementContext.subject
   }
 
   "The license key life cycle" should {
@@ -51,23 +51,23 @@ extends WordSpec { this: TestContext =>
 
       val cs = store
       val cm = consumerManager(cs)
-      cs exists () should equal (false)
+      cs exists () shouldBe false
       intercept[LicenseManagementException] { cm view () }
       intercept[LicenseManagementException] { cm verify () }
       intercept[LicenseManagementException] { cm uninstall () }
       val installed = cm install vs
-      installed should equal (created)
+      installed shouldBe created
       installed should not be theSameInstanceAs (created)
       ;{
         val reinstalled = cm install vs
-        reinstalled should equal (installed)
+        reinstalled shouldBe installed
         reinstalled should not be theSameInstanceAs (installed)
       }
       cm verify ()
       val viewed = cm view ()
-      viewed should equal (created)
+      viewed shouldBe created
       viewed should not be theSameInstanceAs (created)
-      viewed should equal (installed)
+      viewed shouldBe installed
       viewed should not be theSameInstanceAs (installed)
       cm uninstall ()
       intercept[LicenseManagementException] { cm view () }
@@ -78,25 +78,25 @@ extends WordSpec { this: TestContext =>
     "work for FTP license keys" in {
       val cs = store
       val cm = consumerManager(cs)
-      cs exists () should equal (false)
+      cs exists () shouldBe false
       val fcs = store
       val fcm = ftpConsumerManager(cm, fcs)
-      fcs exists () should equal (false)
+      fcs exists () shouldBe false
       fcm verify () // generate
       val generated = fcm view ()
-      cs exists () should equal (false)
-      fcs exists () should equal (true)
+      cs exists () shouldBe false
+      fcs exists () shouldBe true
       check(generated,
             generated.getIssued,
             datePlusDays(generated.getIssued, 1))
       fcm verify ()
       val viewed = fcm view ()
-      viewed should equal (generated)
+      viewed shouldBe generated
       viewed should not be theSameInstanceAs (generated)
       val viewed2 = fcm view ()
-      viewed2 should equal (generated)
+      viewed2 shouldBe generated
       viewed2 should not be theSameInstanceAs (generated)
-      cs exists () should equal (false)
+      cs exists () shouldBe false
     }
 
     "work for chained license keys" in {
@@ -108,8 +108,8 @@ extends WordSpec { this: TestContext =>
       val ccs = store
       val ccm = chainedConsumerManager(cm, ccs)
 
-      cs exists () should equal (false)
-      ccs exists () should equal (false)
+      cs exists () shouldBe false
+      ccs exists () shouldBe false
 
       intercept[LicenseManagementException] { cm view () }
       intercept[LicenseManagementException] { ccm view () }
@@ -123,19 +123,19 @@ extends WordSpec { this: TestContext =>
         }
 
         val installed = ccm install vs // delegates to cm!
-        installed should equal (created)
+        installed shouldBe created
         installed should not be theSameInstanceAs (created)
-        cs exists () should equal (true)
-        ccs exists () should equal (false)
+        cs exists () shouldBe true
+        ccs exists () shouldBe false
         ccm verify ()
         val viewed = ccm view ()
-        viewed should equal (created)
+        viewed shouldBe created
         viewed should not be theSameInstanceAs (created)
         ccm uninstall () // delegates to cm!
       }
 
-      cs exists () should equal (false)
-      ccs exists () should equal (false)
+      cs exists () shouldBe false
+      ccs exists () shouldBe false
 
       ;{
         val created = {
@@ -146,19 +146,19 @@ extends WordSpec { this: TestContext =>
         }
 
         val installed = ccm install vs // installs in ccm!
-        installed should equal (created)
+        installed shouldBe created
         installed should not be theSameInstanceAs (created)
-        cs exists () should equal (false)
-        ccs exists () should equal (true)
+        cs exists () shouldBe false
+        ccs exists () shouldBe true
         ccm verify ()
         val viewed = ccm view ()
-        viewed should equal (created)
+        viewed shouldBe created
         viewed should not be theSameInstanceAs (created)
         ccm uninstall () // uninstalls from ccm!
       }
 
-      cs exists () should equal (false)
-      ccs exists () should equal (false)
+      cs exists () shouldBe false
+      ccs exists () shouldBe false
     }
   }
 }
