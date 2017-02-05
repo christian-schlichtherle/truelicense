7a8bf5b8362a39759e60fd5831fce16db93ffac3
Christian Schlichtherle <christian@schlichtherle.de>
2017-02-05 03:24:50 +0100

    Updates license plates and XML declarations.

--- truelicense-spi/src/main/java/org/truelicense/spi/io/BiosProvider.java
+++ truelicense-spi/src/main/java/org/truelicense/spi/io/BiosProvider.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2005-2017 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+
 package org.truelicense.spi.io;
 
 /**
--- truelicense-spi/src/main/java/org/truelicense/spi/io/PathStore.java
+++ truelicense-spi/src/main/java/org/truelicense/spi/io/PathStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Schlichtherle IT Services & CirrusPoint Solutions, Inc.
+ * Copyright (C) 2005-2017 Schlichtherle IT Services.
  * All rights reserved. Use is subject to license terms.
  */
 package org.truelicense.spi.io;
