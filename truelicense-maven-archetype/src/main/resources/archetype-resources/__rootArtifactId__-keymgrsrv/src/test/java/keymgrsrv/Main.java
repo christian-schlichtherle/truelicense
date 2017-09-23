2f4a9d904f60c794e2f7fcd9a4dc12b543cef757
Christian Schlichtherle <christian@schlichtherle.de>
2017-09-23 16:23:04 +0400

    Renames `keymgrsvc` to `keymgrsrv`.

--- truelicense-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-keymgrsrv/src/test/java/keymgrsrv/Main.java
+++ truelicense-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-keymgrsrv/src/test/java/keymgrsrv/Main.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2005-2015 Schlichtherle IT Services.
  * All rights reserved. Use is subject to license terms.
  */
-package ${package}.keymgrsvc;
+package ${package}.keymgrsrv;
 
 import ${package}.keymgr.LicenseManager;
 import java.io.IOException;
