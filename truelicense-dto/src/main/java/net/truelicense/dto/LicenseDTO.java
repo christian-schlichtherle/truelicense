6175a4a0cda4f2f04c23bf81f4b6152f676cdbcf
Christian Schlichtherle <christian@schlichtherle.de>
2017-10-02 02:10:40 +0400

    Adds documentation and obfuscation support for DTO classes.

--- truelicense-dto/src/main/java/net/truelicense/dto/LicenseDTO.java
+++ truelicense-dto/src/main/java/net/truelicense/dto/LicenseDTO.java
@@ -1,7 +1,15 @@
+/*
+ * Copyright (C) 2005-2017 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+
 package net.truelicense.dto;
 
 import net.truelicense.api.License;
 
+/**
+ * @author Christian Schlichtherle
+ */
 public class LicenseDTO {
 
     public License license;
