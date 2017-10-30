40fb34d71e5e95e3686dc292bb10dea18574a00e
Christian Schlichtherle <christian@schlichtherle.de>
2017-10-30 08:18:47 +0400

    RCB
    (cherry picked from commit ba20696)

--- truelicense-v1/src/main/java/net/truelicense/v1/auth/V1RepositoryController.java
+++ truelicense-v1/src/main/java/net/truelicense/v1/auth/V1RepositoryController.java
@@ -38,18 +38,6 @@ final class V1RepositoryController implements RepositoryController {
         this.codec = requireNonNull(codec);
     }
 
-    private byte[] data(Codec codec, String body) {
-        return charset(codec)
-                .map(body::getBytes)
-                .orElseGet(() -> getDecoder().decode(body));
-    }
-
-    private String body(Codec codec, byte[] artifact) {
-        return charset(codec)
-                .map(cs -> new String(artifact, cs))
-                .orElseGet(() -> getEncoder().encodeToString(artifact));
-    }
-
     @Override
     public final Decoder sign(final Signature engine, final Object artifact) throws Exception {
         final MemoryStore store = new MemoryStore();
@@ -70,6 +58,12 @@ final class V1RepositoryController implements RepositoryController {
         return codec.decoder(store);
     }
 
+    private static String body(Codec codec, byte[] artifact) {
+        return charset(codec)
+                .map(charset -> new String(artifact, charset))
+                .orElseGet(() -> getEncoder().encodeToString(artifact));
+    }
+
     @Override
     public final Decoder verify(final Signature engine) throws Exception {
         if (!engine.getAlgorithm().equalsIgnoreCase(model.getSignatureAlgorithm()))
@@ -79,5 +73,11 @@ final class V1RepositoryController implements RepositoryController {
         if (!engine.verify(getDecoder().decode(model.getSignature())))
             throw new RepositoryIntegrityException();
         return codec.decoder(new MemoryStore().data(artifactData));
+    }
+
+    private static byte[] data(Codec codec, String body) {
+        return charset(codec)
+                .map(body::getBytes)
+                .orElseGet(() -> getDecoder().decode(body));
     }
 }
