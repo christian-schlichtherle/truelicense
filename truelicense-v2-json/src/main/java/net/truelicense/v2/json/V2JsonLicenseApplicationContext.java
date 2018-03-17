7e13ee369a4c4ac1eeb84258ec662dbed15fa8d5
Christian Schlichtherle <christian@schlichtherle.de>
2018-03-17 19:54:29 +0400

    Experimental refactoring to get rid of the JAXB dependency by using a custom `JSONSerializer` and `JSONDeserializer`. Unfortunately, it doesn't help because the `License` class still needs to have JAXB annotations for the JAX-RS module.

--- truelicense-v2-json/src/main/java/net/truelicense/v2/json/V2JsonLicenseApplicationContext.java
+++ truelicense-v2-json/src/main/java/net/truelicense/v2/json/V2JsonLicenseApplicationContext.java
@@ -6,11 +6,21 @@
 package net.truelicense.v2.json;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import net.truelicense.api.LicenseManagementContextBuilder;
 import net.truelicense.v2.commons.V2LicenseApplicationContext;
 import net.truelicense.v2.json.codec.JsonCodec;
+
+import javax.security.auth.x500.X500Principal;
+import java.io.IOException;
 
 /**
  * The root context for applications which need to manage of Version-2-with-JSON (V2/JSON) format license keys.
@@ -27,7 +37,22 @@ public final class V2JsonLicenseApplicationContext extends V2LicenseApplicationC
     private ObjectMapper objectMapper() {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
-        mapper.registerModule(new JaxbAnnotationModule());
+        final SimpleModule module = new SimpleModule();
+        module.addSerializer(new StdSerializer<X500Principal>(X500Principal.class) {
+
+            @Override
+            public void serialize(X500Principal value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+                gen.writeString(value.getName());
+            }
+        });
+        module.addDeserializer(X500Principal.class, new StdDeserializer<X500Principal>(X500Principal.class) {
+
+            @Override
+            public X500Principal deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+                return new X500Principal(p.readValueAs(String.class));
+            }
+        });
+        mapper.registerModule(module);
         return mapper;
     }
 }
